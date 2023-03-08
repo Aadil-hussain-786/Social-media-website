71a93427e1d7c1de8ef5c9491379ec5203142709
Aadil Hussain <127195496+Aadil-hussain-786@users.noreply.github.com>
2023-03-07 21:21:29 -0800

    Add files via upload

---
+++ src/pages/GroupComponent1.js
@@ -0,0 +1,58 @@
+const GroupComponent1 = () => {
+  return (
+    <div className="relative w-full h-[827px]">
+      <img
+        className="absolute top-[15px] left-[0px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../01--onboarding@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[406px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../02--sign-in@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[812px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../03--menus@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[1218px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../04--followers@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[1624px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../05--chat@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[2030px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../06--profile@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[2436px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../07--phonebook@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[2842px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../08--notification@2x.png"
+      />
+      <img
+        className="absolute top-[15px] left-[3248px] w-[375px] h-[812px] object-cover"
+        alt=""
+        src="../09--comments@2x.png"
+      />
+      <img
+        className="absolute top-[0px] left-[3654px] w-[375px] h-[827px] object-cover"
+        alt=""
+        src="../10--timeline@2x.png"
+      />
+    </div>
+  );
+};
+
+export default GroupComponent1;
